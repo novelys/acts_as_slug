8b9278e7145a41caea2a66342d9dd85447faf6da
slainer68 <slainer68@gmail.com>
2009-03-05 20:08:20 +0100

    Deletes string_ext, use Novelys_hacks!

--- lib/string_ext.rb
+++
@@ -1,8 +0,0 @@
-require 'unicode'
-
-class String
-  def to_slug
-    str = Unicode.normalize_KD(self).gsub(/[^\x00-\x7F]/n,'')
-    str = str.gsub(/\W+/, '-').gsub(/^-+/,'').gsub(/-+$/,'').downcase
-  end
-end
