4aad08a2ac20278f8af36763597374d4ed0c09d8
slainer68 <slainer68@gmail.com>
2009-03-24 17:31:10 +0100

    slug_utilities methods should be class methods

--- lib/slug_utilities.rb
+++ lib/slug_utilities.rb
@@ -1,9 +1,9 @@
 class SlugUtilities
-  def base_from_slug(slug)
+  def self.base_from_slug(slug)
     return slug =~ /\A(.*)-([0-9]+)\Z/ ? slug.slice(/\A(.*)-([0-9]+)\Z/, 1) : slug
   end
 
-  def id_from_slug(slug)
+  def self.id_from_slug(slug)
     return slug if slug =~ /\A[0-9]+\Z/
     return slug =~ /\A(.*)-([0-9]+)\Z/ ? slug.slice(/\A(.*)-([0-9]+)\Z/, 2) : nil
   end
