2d3d785a4564cf4ba4b08c51a55fed674974e4bd
David Bourguignon <david.bourguignon@novelys.com>
2009-09-14 10:01:34 +0200

    optimization by using slug column even for append_id slugs.
    You should add an index on the slug column

--- lib/acts_as_slug.rb
+++ lib/acts_as_slug.rb
@@ -7,7 +7,6 @@ module ActiveRecord
     
       module ClassMethods
         def acts_as_slug(slug_array, options = {})
-          
           raise "You need novelys_hacks plugin in order to use the acts_as_slug plugin" unless String.new.respond_to?(:to_slug)
           
           write_inheritable_attribute(:slug_array, slug_array)
@@ -16,12 +15,18 @@ module ActiveRecord
           class_inheritable_reader :slug_options
           
           def find_with_slug!(slug, *args)
-            if slug_options[:append_id]
-              res = find(SlugUtilities.id_from_slug(slug), *args)
-              raise SlugError, res unless res.to_slug == slug
-            else
-              res = find_by_slug(slug)
-              raise ActiveRecord::RecordNotFound if res.nil?
+            raise ActiveRecord::RecordNotFound unless slug.present?
+            res = find_by_slug(slug)
+            if res.nil?
+              if slug_options[:append_id]
+                res = find(SlugUtilities.id_from_slug(slug), *args)
+                if res && res.slug.blank?
+                  res.update_slug!
+                end
+                raise SlugError, res unless res.slug == slug
+              else
+                raise ActiveRecord::RecordNotFound
+              end
             end
             res.current_slug
             res
@@ -36,14 +41,12 @@ module ActiveRecord
       end
     
       module InstanceMethods
-
+        
         def before_save
-          unless slug_options[:append_id] == true
-            new_slug = self.to_slug
-            conditions = self.new_record? ? ["slug like ?", new_slug + "%"] : ["slug like ? and id <> ?", new_slug + "%", self.id]
-            elements = self.class.find(:all, :conditions => conditions, :order => "slug ASC").find_all {|o| o.slug_base == self.slug_base }
-            self.slug = elements.empty? ? new_slug : new_slug + "-" + (elements.size + 1).to_s
-          end
+          new_slug = self.make_slug
+          conditions = self.new_record? ? ["slug like ?", new_slug + "%"] : ["slug like ? and id <> ?", new_slug + "%", self.id]
+          elements = self.class.find(:all, :conditions => conditions, :order => "slug ASC").find_all {|o| o.slug_base == SlugUtilities.base_from_slug(new_slug) }
+          self.slug = elements.empty? ? new_slug : new_slug + "-" + (elements.size + 1).to_s
         end
         
         def after_save
@@ -55,28 +58,42 @@ module ActiveRecord
         end
         
         def current_slug
-          @current_slug ||= slug_options[:append_id] ? self.to_slug : self.slug
+          self.update_slug! if self.slug.blank? 
+          @current_slug ||= self.slug
         end
         
-        def to_slug
+        def make_slug
           arbitrary_string = /\A\/(.*)\/\Z/
-          evaluated_slug_array = slug_array.inject([]) { |m, v| m << (v =~ arbitrary_string ? v.slice(arbitrary_string, 1) : eval("self.#{v.to_s}.to_slug") rescue ""); m }
+          evaluated_slug_array = slug_array.inject([]) { |m, v| m << (v =~ arbitrary_string ? v.slice(arbitrary_string, 1) : eval("#{v.to_s}.to_slug") rescue ""); m }
 
           base_slug = case slug_options[:separator]
                       when nil then evaluated_slug_array.join "-"
                       when false then evaluated_slug_array.join ""
                       else evaluated_slug_array.join(slug_options[:separator])
                       end
-          
-          slug_options[:append_id] == true ? "#{base_slug}-#{self.id}" : base_slug
+          if slug_options[:append_id] == true
+            if base_slug.length + self.id.to_s.length + 1 > 254
+              base_slug = base_slug[0..254]
+              base_slug[-(self.id.to_s.length + 1), (self.id.to_s.length + 1)] = '-' + self.id.to_s
+            else
+              base_slug = "#{base_slug}-#{self.id}"
+            end
+          end
+          base_slug
+        end
+
+        def update_slug!
+          self.update_attribute :slug, make_slug
+        rescue
+          make_slug
         end
         
         def slug_base
-          SlugUtilities.base_from_slug(current_slug)
+          SlugUtilities.base_from_slug(self.current_slug)
         end
         
         def slug_id
-          SlugUtilities.id_from_slug(current_slug)
+          SlugUtilities.id_from_slug(self.current_slug)
         end
       end #module InstanceMethods
     end #module Related
